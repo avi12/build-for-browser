7ae3c8ae289558506afaaab716f4cac6651cc373
avi12 <avi6106@gmail.com>
2022-02-07 23:37:23 +0200

    Fixed zip creation; removed logging

--- index.js
+++ index.js
@@ -103,9 +103,8 @@ function rebuildZipForBrowser(zipName, version) {
 
   zip.addFile('manifest.json', Buffer.from(JSON.stringify(manifest), 'utf-8'));
 
-  const zipNameOutput = zipName.replace(version, `${version}__adapted_for_${argv.browser}`);
+  const zipNameOutput = zipName.replace(`${version}.zip`, `${version}__adapted_for_${argv.browser}`);
   if (fs.existsSync(zipNameOutput)) {
-    console.log("Removed", zipNameOutput);
     fs.unlinkSync(zipNameOutput);
   }
 
